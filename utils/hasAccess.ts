5a170732bda73838b266609d60bbfd48f1099562
cobalteek <ravshankenz@gmail.com>
2026-03-12 04:23:18 +0500

    Updated hasAccess.ts for universal use

---
+++ utils/hasAccess.ts
@@ -0,0 +1,30 @@
+import type {Project} from "~~/types/project"
+import {useAuthStore} from "~/stores/auth"
+import {storeToRefs} from "pinia"
+import type {Task} from "~~/types/task";
+
+export function hasAccess(
+  {
+    project,
+    task,
+    roles = []
+  }: {
+    project?: Project
+    task?: Task
+    roles?: string[]
+  }) {
+  const obj = ref()
+  if (project) {
+    obj.value = project.createdById
+  } else if (task) {
+    obj.value = task.handler
+  }
+
+  const auth = useAuthStore()
+  const {user} = storeToRefs(auth)
+
+  return (
+    obj.value === user.value?.id ||
+    roles.includes(user.value?.role ?? '')
+  )
+}
