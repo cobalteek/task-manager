5b8ab1c958fd4cd9febfab0369573a8b6b8a62d3
cobalteek <ravshankenz@gmail.com>
2026-03-16 01:53:39 +0500

    Added NotFound.vue

---
+++ app/components/NotFound.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const targetText = `<${$t('error.notFound')}/>`
+</script>
+
+<template>
+  <div class="flex items-center justify-center h-full">
+    <div class="font-mono text-2xl text-gray-100">
+      {{targetText}}
+    </div>
+  </div>
+</template>
